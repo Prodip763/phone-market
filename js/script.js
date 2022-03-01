5493443d813be08b44f16d2b05b64c15e79833c0
prodip763 <prodip12763@gmail.com>
2022-03-02 02:23:41 +0600

    second-commit

--- js/script.js
+++ js/script.js
@@ -1,96 +1,56 @@
 const searchPhone = () => {
-        const searchField = document.getElementById('search-field');
-        const searchText = searchField.value;
-        searchField.value = '';
-        // console.log(searchText);
-        const url = `
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    searchField.value = '';
+    // console.log(searchText);
+    const url = `
     https://openapi.programming-hero.com/api/phones?search=${searchText}
     `;
-        fetch(url)
-            .then(res => res.json())
-            .then(data => displayPhone(data.data[0]));
-    }
-    // searchPhone();
-const displayPhone = phones => {
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhone(data.data));
+}
+
+const displayPhone = phone => {
     const searchResult = document.getElementById('search-result');
-    searchResult.textContent = '';
-    // console.log();
-    phones.forEach(phone => {
+    // console.log(phone);
+    // searchResult.textContent = '';
+    phone.forEach(phone => {
+        // console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-                <div onclick="phoneDetails(${phone.brand})" class="card h-100">
-                    <img src="${phone.image}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">${phone.phone_name}</h5>
-                        <p class="card-text">${phone.slug}</p>
-                    </div>
-                </div>
-            `;
+        <div onclick="loadPhoneDetail(${phone.brand})" class="card h-100">
+        <img src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${phone.phone_name}</h5>
+          <p class="card-text">${phone.slug}</p>
+        </div>
+        </div>
+                `;
         searchResult.appendChild(div);
 
-    });
+    })
 }
-
-
-
-
-
-// const searchFood = () => {
-//     const searchField = document.getElementById('search-field');
-//     const searchText = searchField.value;
-//     searchField.value = '';
-//     if (searchText == '') {
-//         // please write something
-//         console.log('there is no food');
-//     } else {
-//         const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
-//         fetch(url)
-//             .then(response => response.json())
-//             .then(data => displayFood(data.meals));
-//     }
-// }
-
-// const displayFood = meals => {
-//     const searchResult = document.getElementById('search-result');
-//     searchResult.textContent = '';
-//     // if (meals.length == 0) {
-//     //     // show no result found
-//     // }
-//     meals.forEach(meal => {
-//         // console.log(meal);
-//         const div = document.createElement('div');
-//         div.classList.add('col');
-//         div.innerHTML = `
-//                 <div onclick="foodDetails(${meal.idMeal})" class="card h-100">
-//                     <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
-//                     <div class="card-body">
-//                         <h5 class="card-title">${meal.strMeal}</h5>
-//                         <p class="card-text">${meal.strInstructions.slice(0, 250)}</p>
-//                     </div>
-//                 </div>
-//             `;
-//         searchResult.appendChild(div);
-//     })
-// }
-// const foodDetails = mealId => {
-//     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
-//     fetch(url)
-//         .then(response => response.json())
-//         .then(data => displayFoodDetail(data.meals[0]));
-// }
-// const displayFoodDetail = meal => {
-//     console.log(meal);
-//     const mealDetails = document.getElementById('meal-details');
-//     const div = document.createElement('div');
-//     div.classList.add('card');
-//     div.innerHTML = `
-//                 <img src="${meal.strMealThumb}" class="card-img-top " alt="... ">
-//                 <div class="card-body ">
-//                     <h5 class="card-title ">${meal.strMeal}</h5>
-//                     <p class="card-text ">${meal.strTnstructions.slice(0, 150)}</p>
-//                     <a href="${meal.strYoutube}" class="btn btn-primary ">Go somewhere</a>
-//                 </div>
-//                 `;
-//     mealDetails.appendChild(div);
-// }
+const loadPhoneDetail = Id => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayPhoneDetail(data.data));
+}
+const displayPhoneDetail = phone => {
+    console.log(phone);
+    const mealDetails = document.getElementById('phone-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img src="${phone.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${phone.phone_name}</h5>
+      <p class="card-text">${phone.slug}</p>
+      <a href=" <h2>Other Details</h2>${phone.mainFeatures.displaySize}, ${phone.mainFeatures.memory}, ${phone.releaseDate}" class="btn btn-primary ">Go somewhere</a>
+                </div>
+                `;
+    mealDetails.appendChild(div);
+}
